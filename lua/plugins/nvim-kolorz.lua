a2a9ca2fc6c1e6033dc5f5d8a1f4f2ec92250433
Keyoonz <bonnardmae@gmail.com>
2025-11-18 17:29:32 +0100

    adding nvim implementation by default in konfiz

--- lua/plugins/nvim-kolorz.lua
+++ lua/plugins/nvim-kolorz.lua
@@ -4,6 +4,9 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("nvim-kolorz").setup({
+			konfiz_integrated = true,
+		})
 		vim.cmd("colorscheme nvim-kolorz")
 	end,
 }
