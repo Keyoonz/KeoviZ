34619114ed28534e7a147598c230d5ff5acd6cab
Keyoonz <bonnardmae@gmail.com>
2025-11-19 01:57:34 +0100

    setup rust

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,28 +1,35 @@
 return {
-  {
-    'mason-org/mason.nvim',
-    opts = {},
-  },
-  {
-    'mason-org/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = {
-        'lua_ls',
-        'clangd',
-        'harper_ls',
-      },
-      automatic_enable = {
-        exclude = {
-          'harper_ls',
-        },
-      },
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-		 -- local lspconfig = require 'lspconfig'
-      vim.lsp.config('harper_ls', { filetypes = { 'markdown' }})
-    end,
-  },
+	{
+		'mason-org/mason.nvim',
+		opts = {},
+	},
+	{
+		'mason-org/mason-lspconfig.nvim',
+		opts = {
+			ensure_installed = {
+				'lua_ls',
+				'clangd',
+				'harper_ls',
+			},
+			automatic_enable = {
+				exclude = {
+					'harper_ls',
+				},
+			},
+		},
+	},
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			-- local lspconfig = require 'lspconfig'
+			vim.lsp.config('harper_ls', { filetypes = { 'markdown' }})
+			vim.lsp.config('rust_analyser', { filetypes = { 'rust' }})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			-- options
+		},
+	}
 }
